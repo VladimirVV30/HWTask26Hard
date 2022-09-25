93d7dc7551f4e069d98f49a737fadb41be3e290e
Vladimir <madfrog@e1.ru>
2022-09-25 22:12:41 +0300

    Created Task 26Hard

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход число (целое или вещественное) и выдаёт количество цифр в числе.
+// 456 -> 3
+// 0.78 -> 2
+// 89.126 -> 5
+
+
+int SumNums (double N)
+{
+    int i = 0;
+    double x = N;
+
+    while (x % 1 > 0)
+    {
+        x = x * 10;
+    }
+
+    while (x > 0)
+    {
+        x = Convert.ToInt32(x / 10);
+        i++;
+    }
+    return i;
+}
+try
+{
+    System.Console.Write("Введите число ");
+    double a = Convert.ToDouble (Console.ReadLine());
+    System.Console.WriteLine("Число "+ a +" состоит из "+ SumNums(a) +" цифр");
+
+}
+catch
+{
+    System.Console.WriteLine("Необходимо ввести число цифрами!");
+}
